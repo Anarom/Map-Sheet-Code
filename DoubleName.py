8c03f8e94ec5b2db92a486800a21331845dfa9ca
Anarom <forprojectdi@gmail.com>
2018-08-09 00:51:08 +0400

    Added plain feature with TODO text

---
+++ DoubleName.py
@@ -0,0 +1,5 @@
+#Every time before pushing name to main.self.code.set() it will be loaded
+#into this function as well as main arguments (latitude, longitude, scale, hemisphere)
+
+#Maybe it is better to do from calculate() before name generation
+#Should check name generation rules for further progress
