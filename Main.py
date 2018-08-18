1964f89875b35fb7165ac9ea7b6c5969477ad475
Anarom <forprojectdi@gmail.com>
2018-08-18 15:53:01 +0400

    Rewrote Main

--- Main.py
+++ Main.py
@@ -4,66 +4,57 @@ from Calculate import *
 from Widgets import *
 
 class Main_Window():
+    
     def __init__(self):
-        self.root = Tk()
-        self.code = StringVar(self.root)
-        self.widgets = ['Latitude', 'Longitude', 'Scale', 'Hemisphere', '', '']
-        self.windows = [0,0,0,0,0,0,0]
-        self.root.title('Map sheet nomenclature')
-        self.root.resizable(False, False)
-        self.root.geometry('730x180+350+250')
-        self.root.bind('<Return>', self.calc) 
-        self.windows[0] = Coord_Window(self.root, 120, 10, 90)
-        self.windows[1] = Coord_Window(self.root, 120, 50, 180)
-        self.windows[2] = Scale_Window(self.root, 120, 85)
-        self.windows[3] = Hemisphere_Window(self.root, 120, 125)
-        self.windows[4] = Border_Window(self.root, 500, 40)
-        self.windows[5] = Border_Img(self.root, 0)
-        self.windows[6] = Border_Img(self.root, 1)
-        for index in range(0, 4):
-            self.widgets[index] = Label(self.root, text = self.widgets[index])
-            self.widgets[index].place(x = 60,
-                                      y = 20 + index * 40,
-                                      height = 20,
-                                      anchor = CENTER)
+        root = self.setup()
+        root.mainloop()
 
-        self.widgets[4] = Entry(self.root,
-                                state = 'readonly',
-                                textvariable = self.code)
-
-        self.widgets[4].place(x = 350,
-                              y = 40,
-                              width = 165,
-                              height = 20,
-                              anchor = CENTER)
-
-        self.widgets[5] = Button(self.root, text = 'Calculate')
-        self.widgets[5].bind('<Button-1>', self.calc)
-        self.widgets[5].place(x = 310, y = 100, width = 80, height = 40)
-        self.windows[6].canvas.place(x = 500, y = 35, width = 140, height = 100)
-        self.root.mainloop()
-
-    def calc(self, event):
-        if self.windows[3].get_value() > 1:
-            self.windows[5].canvas.place(x = 500, y = 35, width = 140, height = 100)
-            self.windows[6].canvas.place_forget()
+    def set_event(self, event):
+        values = [instance.get_value() for instance in self.instances[:4]]
+        if values[3] > 1:
+            self.instances[5].canvas.place(x = 500, y = 35, width = 140, height = 100)
+            self.instances[6].canvas.place_forget()
         else:
-            self.windows[5].canvas.place_forget()
-            self.windows[6].canvas.place(x = 500, y = 35, width = 140, height = 100)
-
-        self.code.set(generate_name(self.windows[0].get_value(),
-                                self.windows[1].get_value(),
-                                self.windows[2].get_value(),
-                                self.windows[3].get_value()))
+            self.instances[5].canvas.place_forget()
+            self.instances[6].canvas.place(x = 500, y = 35, width = 140, height = 100)
+        self.code.set(generate_name(*values))
         for index in range(0,4):
-            self.coord =  corners(self.windows[0].get_value(),
-                                  self.windows[1].get_value(),
-                                  self.windows[2].get_value(),
-                                  self.windows[3].get_value(),
-                                  index)
-            self.deg = self.coord // 60
-            self.min = self.coord - self.deg * 60
-            self.sec = (self.min - int(self.min)) * 60
-            self.windows[4].str_list[index].set(f'{int(self.deg)}° {int(self.min)}′ {round(self.sec)}′′')
+            coord =  corners(*values, index)
+            deg = coord // 60
+            min = coord - deg * 60
+            sec = (min - int(min)) * 60
+            self.instances[4].str_list[index].set(f'{int(deg)}° {int(min)}′ {round(sec)}′′')
+        
+    def setup(self):
+        root = Tk()
+        root.title('Map sheet nomenclature')
+        root.resizable(False, False)
+        root.geometry('730x180+350+250')
+        root.bind('<Return>', self.set_event)
+        self.code = StringVar(root)
+        self.instances = [0,0,0,0,0,0,0]
+        self.instances[0] = Coord_Window(root, 120, 10, 90)
+        self.instances[1] = Coord_Window(root, 120, 50, 180)
+        self.instances[2] = Scale_Window(root, 120, 85)
+        self.instances[3] = Hemisphere_Window(root, 120, 125)
+        self.instances[4] = Border_Window(root, 500, 40)
+        self.instances[5] = Border_Img(root, 0)
+        self.instances[6] = Border_Img(root, 1)
+        self.instances[6].canvas.place(x = 500, y = 35, width = 140, height = 100)
+    
+        widgets = ['Latitude', 'Longitude', 'Scale', 'Hemisphere', '', '']
+        for index in range(0, 4):
+            widgets[index] = Label(root, text = widgets[index])
+            widgets[index].place(x = 60, y = 20 + index * 40,
+                                 height = 20, anchor = CENTER)
+        
+        widgets[4] = Entry(root, state = 'readonly', textvariable = self.code)
+        widgets[4].place(x = 350, y = 40, width = 165,
+                         height = 20, anchor = CENTER)
+    
+        widgets[5] = Button(root, text = 'Calculate')
+        widgets[5].bind('<Button-1>', self.set_event)
+        widgets[5].place(x = 310, y = 100, width = 80, height = 40)
+        return root
 
 window = Main_Window()
