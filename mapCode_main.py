4b8d08240b25cb397ec3418017785720996960d2
Anarom <forprojectdi@gmail.com>
2019-02-18 13:37:23 +0300

    Added int validator to input strings

--- mapCode_main.py
+++ mapCode_main.py
@@ -1,11 +1,27 @@
-import design
 import sys
+
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QIntValidator
+import design
+
 
 class Connect(QtWidgets.QMainWindow, design.Ui_MainWindow):
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        
+        lst = []
+        int_val = QIntValidator()
+        for widget in self.findChildren(QtWidgets.QLineEdit):
+            if not widget.isReadOnly():
+                widget.setValidator(int_val)
+                widget.textChanged.connect(self.debug_output)
+                lst.append(widget)
+            
+    def debug_output(self):
+        sender = self.sender()
+        print('debug output ',sender.text())
 
 
 app = QtWidgets.QApplication(sys.argv)
